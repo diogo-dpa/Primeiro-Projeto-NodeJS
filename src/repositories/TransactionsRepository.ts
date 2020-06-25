96aaf859547e71a45c7b54431fc8d41f9136be78
Diogo Almazan <diogodpa@outlook.com>
2020-06-24 22:52:33 -0300

    Functionalities implemented

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,12 @@
 import Transaction from '../models/Transaction';
 
+// Create a DTO to help formating params in Create method
+interface CreateTransactioDTO{
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +21,40 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+
+    // Return the array
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    
+    // sum the income values 
+    const income = this.transactions.reduce( (acumulator, current) => current.type==="income"? 
+                                                        acumulator + current.value: acumulator, 0);
+    // sum the outcome values
+    const outcome = this.transactions.reduce( (acumulator, current) => current.type==="outcome"? 
+                                                        acumulator + current.value: acumulator, 0);
+    // Solve the total
+    const total = income - outcome;
+    // Create an object to return
+    const balance = {
+      income,
+      outcome,
+      total
+    }
+    
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title, value, type}: CreateTransactioDTO): Transaction {
+    
+    // Before add the new transaction, it needed to valid it 
+    const transaction = new Transaction({title, value, type})
+    // Add the transaction into the array
+    this.transactions.push(transaction)
+
+    // return the new transaction
+    return transaction;
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,27 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+// Realiza a instância do repositório
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
     // TODO
+
+    const transactions = transactionsRepository.all()
+    const balance = transactionsRepository.getBalance()
+
+    const resultBalance = {
+      transactions,
+      balance
+    }
+
+    return response.json(resultBalance)
+
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,6 +29,14 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
+
+    const {title, value, type} = request.body;
+
+    const createTransaction = new CreateTransactionService(transactionsRepository);
+    const transaction = createTransaction.execute({title, value, type})
+
+    return response.json(transaction)
+
     // TODO
   } catch (err) {
     return response.status(400).json({ error: err.message });
